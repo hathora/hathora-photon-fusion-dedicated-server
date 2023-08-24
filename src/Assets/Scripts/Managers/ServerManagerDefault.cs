92c01be4f3da5a40c3afdd46d7585582af122a49
Dylan Hunt <dylan@imperium42.com>
2023-08-24 16:16:07 +0800

    chore: Revert ServerManagerDefault + Obsolete tag
    This is a Photon file - we no longer use it

--- src/Assets/Scripts/Managers/ServerManagerDefault.cs
+++ src/Assets/Scripts/Managers/ServerManagerDefault.cs
@@ -1,3 +1,4 @@
+using System;
 using Fusion.Sample.DedicatedServer.Utils;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -5,62 +6,31 @@ using UnityEngine.SceneManagement;
 
 namespace Fusion.Sample.DedicatedServer
 {
+    [Obsolete("For the Hathora<>Fusion demo, instead use `HathoraPhotonServerMgrDefault`")]
+    public class ServerManagerDefault : ServerManagerBase
+    {
 
-  /// <summary>
-  /// Hathora edits:
-  /// - Add `|| UNITY_EDITOR` to `#if UNITY_SERVER` to allow optional Editor debugging as server.
-  /// - Added server + client wrappers for clarity, since #if wrappers get confusing.
-  /// </summary>
-  public class ServerManagerDefault : ServerManagerBase
-  {
-    #region Start Wrapper
-    // ########################################################################################
-    private async void Start()
-    {
-      await Task.Yield();
-      await OnStart();
-    }
-    
-    protected virtual async Task OnStart()
-    {
+        async void Start()
+        {
+            await Task.Yield();
+
 #if UNITY_SERVER
-      await LoadGameAsDedicatedServer();
-#else
-      LoadMenuAsClient();
-#endif
-    }
-    // ########################################################################################
-    #endregion // Start Wrapper
-
-
-    #region Utils
-    /// <summary>Load scene 1 (Menu) as Client</summary>
-    protected virtual void LoadMenuAsClient()
-    {
-      Debug.Log("[ServerManagerDefault] loadMenuAsClient (`1.Menu` scene)");
-      SceneManager.LoadScene((int)SceneDefs.MENU, LoadSceneMode.Single);
-    }
-
-    /// <summary>Load scene 2 (Game) as Client</summary>
-    protected virtual async Task LoadGameAsDedicatedServer()
-    {
       // Continue with start the Dedicated Server
-      Debug.Log("[ServerManagerDefault] loadGameAsDedicatedServer (`2.Game` scene)");
       Application.targetFrameRate = 30;
 
-      DedicatedServerConfig config = DedicatedServerConfig.Resolve();
+      var config = DedicatedServerConfig.Resolve();
       Debug.Log(config);
 
       // Start a new Runner instance
-      NetworkRunner runner = Instantiate(_runnerPrefab);
+      var runner = Instantiate(_runnerPrefab);
 
       // Start the Server
-      StartGameResult result = await StartSimulation(runner, config);
+      var result = await StartSimulation(runner, config);
 
       // Check if all went fine
-      if (result.Ok) { Log.Debug($"Runner Start DONE"); }
-      else
-      {
+      if (result.Ok) {
+        Log.Debug($"Runner Start DONE");
+      } else {
         // Quit the application if startup fails
         Log.Debug($"Error while starting Server: {result.ShutdownReason}");
 
@@ -68,8 +38,9 @@ namespace Fusion.Sample.DedicatedServer
         // using 0 means all went fine
         Application.Quit(1);
       }
+#else
+            SceneManager.LoadScene((int)SceneDefs.MENU, LoadSceneMode.Single);
+#endif
+        }
     }
-    #endregion // Utils
-
-  }
 }
